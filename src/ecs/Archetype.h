7d48a0943c474c6b165f5c2296c194121e75045f
Quarter-Quarten <3431442688@qq.com>
2026-05-31 14:59:29 +0800

    compile

--- src/ecs/Archetype.h
+++ src/ecs/Archetype.h
@@ -4,16 +4,11 @@
 
 #include <bitset>
 
-struct Archetype {
-	std::bitset<kMaxComponentCount> component_mask;
-	std::vector<ComponentMeta*> components;
-	
+class Archetype final {
+public:
 	size_t entity_stride;
 	size_t capacity;
-	
-	// Chunk本体在连续数组中 Chunk对齐为16kb
-	std::vector<Chunk*> chunks;
-	
+
 	Archetype(
 		std::bitset<kMaxComponentCount> p_component_mask,
 		std::vector<ComponentMeta*> p_components
@@ -28,6 +23,16 @@ struct Archetype {
     void add_entity();
     // 删除指定索引的实体
     void remove_entity(std::uint16_t chunk_index, std::uint16_t index);
+
+private:
+	// 位标记 一位的0/1表示该Archetype是否有该组件 用于检索
+	std::bitset<kMaxComponentCount> component_mask;
+	// 紧密数组 所有的组件
+	std::vector<ComponentMeta*> components;
+
+	// Chunk中的数据以SoA排布方式存储 此数组缓存每种组件的起始偏移
+	std::vector<uint16_t> offsets;
 	
-	
+	// Chunk本体在全局连续数组中 Chunk对齐为16kb
+	std::vector<Chunk*> chunks;
 };
--- src/ecs/chunk.cpp
+++ src/ecs/chunk.cpp
@@ -5,7 +5,7 @@ void Chunk::recreate(Archetype* p_type) {
 	type = p_type;
 }
 
-
+//std::uint16_t Chunk::add_entity
 
 
 
