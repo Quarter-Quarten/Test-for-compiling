569a347d1589c8a5f3824575e2ac86c69874805b
Quarter-Quarten <3431442688@qq.com>
2026-06-09 13:02:55 +0800

    compile

--- src/register_types.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <godot_cpp/core/class_db.hpp>
-
-using namespace godot;
-
-void open_tower_library_init(ModuleInitializationLevel p_level);
-void open_tower_uninitialize_module(ModuleInitializationLevel p_level);
