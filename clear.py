312558e350b218563105bbd0d4bf8a0391a0fda7
Quarter-Quarten <3431442688@qq.com>
2026-06-13 12:49:36 +0800

    compile

--- clear.py
+++ clear.py
@@ -3,3 +3,7 @@ for f in Path('.').rglob('*.o'):
     f.unlink()
 for f in Path('.').rglob('*.os'):
     f.unlink()
+for f in Path('.').rglob('*.cpp.bak'):
+    f.unlink()
+for f in Path('.').rglob('*.hpp.bak'):
+    f.unlink()
